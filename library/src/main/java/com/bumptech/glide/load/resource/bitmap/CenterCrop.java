37626825c09348b97ea47109dd3edee75c8aa0ee
AdityaPrakashGoogle <aadityaprakash@google.com>
2025-02-14 08:34:09 +0000

    Ignore BitmapPool in TransformationUtils in fitcenter and centercrop

--- library/src/main/java/com/bumptech/glide/load/resource/bitmap/CenterCrop.java
+++ library/src/main/java/com/bumptech/glide/load/resource/bitmap/CenterCrop.java
@@ -18,8 +18,8 @@ public class CenterCrop extends BitmapTransformation {
 
   @Override
   protected Bitmap transform(
-      @NonNull BitmapPool pool, @NonNull Bitmap toTransform, int outWidth, int outHeight) {
-    return TransformationUtils.centerCrop(pool, toTransform, outWidth, outHeight);
+      @NonNull BitmapPool pool, @NonNull Bitmap toTransform, int outWidth, int outHeight,boolean useCreateBitmapToPreserveGainmap) {
+    return TransformationUtils.centerCrop(pool, toTransform, outWidth, outHeight,useCreateBitmapToPreserveGainmap);
   }
 
   @Override
